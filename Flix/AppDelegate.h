0beaacdc202d44a57513a44e1473ddb373d2dd09
Mario Baxter <mariobaxter@fb.com>
2019-06-26 11:12:02 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by mariobaxter on 6/26/19.
+//  Copyright © 2019 mariobaxter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
